9643f2e32d63bc48d22d7182938dbe0f0f54ca74
meew12 <meeworkout@gmail.com>
2025-02-15 00:49:06 -0300

    Add files via upload

---
+++ js/5870369.js
@@ -0,0 +1,8 @@
+// HubSpot Script Loader. Please do not block this resource. See more: http://hubs.ly/H0702_H0
+
+!function(e,t){if(!document.getElementById(e)){var c=document.createElement("script");c.src="https://js.hs-analytics.net/analytics/1739588700000/5870369.js",c.type="text/javascript",c.id=e;var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(c,n)}}("hs-analytics");
+!function(t,e,r){if(!document.getElementById(t)){var n=document.createElement("script");for(var a in n.src="https://js.hsleadflows.net/leadflows.js",n.type="text/javascript",n.id=t,r)r.hasOwnProperty(a)&&n.setAttribute(a,r[a]);var i=document.getElementsByTagName("script")[0];i.parentNode.insertBefore(n,i)}}("LeadFlows-5870369",0,{"crossorigin":"anonymous","data-leadin-portal-id":5870369,"data-leadin-env":"prod","data-loader":"hs-scriptloader","data-hsjs-portal":5870369,"data-hsjs-env":"prod","data-hsjs-hublet":"na1"});
+var _hsp = window._hsp = window._hsp || [];
+_hsp.push(['addEnabledFeatureGates', []]);
+_hsp.push(['setBusinessUnitId', 0]);
+!function(t,e,r){if(!document.getElementById(t)){var n=document.createElement("script");for(var a in n.src="https://js.hs-banner.com/5870369.js",n.type="text/javascript",n.id=t,r)r.hasOwnProperty(a)&&n.setAttribute(a,r[a]);var i=document.getElementsByTagName("script")[0];i.parentNode.insertBefore(n,i)}}("cookieBanner-5870369",0,{"data-cookieconsent":"ignore","data-hs-ignore":true,"data-loader":"hs-scriptloader","data-hsjs-portal":5870369,"data-hsjs-env":"prod","data-hsjs-hublet":"na1"});
